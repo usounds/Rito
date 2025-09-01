db349656ce01f6f0a6fe2d09c34714c61c1548e7
usounds <46260022+usounds@users.noreply.github.com>
2025-09-01 20:58:37 +0900

    feat : CDN cache

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,35 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
-// next-intl のプラグイン初期化
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
+
+  async headers() {
+    return [
+      {
+        source: "/_next/static/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public,max-age=31536000,immutable",
+          },
+        ],
+      },
+      {
+        source: "/_next/data/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public,max-age=0,must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
-// next-intl プラグインを適用
 export default withNextIntl(nextConfig);

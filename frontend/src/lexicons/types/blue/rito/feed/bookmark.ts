ebf88994119d4d038de7d520cb5965c2f5953acc
usounds <grave.evarg@gmail.com>
2025-09-02 19:42:03 +0900

    feat : 多言語対応

--- frontend/src/lexicons/types/blue/rito/feed/bookmark.ts
+++ frontend/src/lexicons/types/blue/rito/feed/bookmark.ts
@@ -2,21 +2,23 @@ import type {} from "@atcute/lexicons";
 import * as v from "@atcute/lexicons/validations";
 import type {} from "@atcute/lexicons/ambient";
 
+const _localeSchema = /*#__PURE__*/ v.object({
+  $type: /*#__PURE__*/ v.optional(
+    /*#__PURE__*/ v.literal("blue.rito.feed.bookmark#locale"),
+  ),
+  comment: /*#__PURE__*/ v.optional(/*#__PURE__*/ v.string()),
+  lang: /*#__PURE__*/ v.literalEnum(["en", "ja"]),
+  title: /*#__PURE__*/ v.string(),
+});
 const _mainSchema = /*#__PURE__*/ v.record(
   /*#__PURE__*/ v.tidString(),
   /*#__PURE__*/ v.object({
     $type: /*#__PURE__*/ v.literal("blue.rito.feed.bookmark"),
     createdAt: /*#__PURE__*/ v.datetimeString(),
-    publicComment: /*#__PURE__*/ v.optional(
-      /*#__PURE__*/ v.constrain(/*#__PURE__*/ v.string(), [
-        /*#__PURE__*/ v.stringLength(0, 20000),
-        /*#__PURE__*/ v.stringGraphemes(0, 2000),
-      ]),
-    ),
     get resolvers() {
       return /*#__PURE__*/ v.optional(/*#__PURE__*/ v.array(nsidSchema));
     },
-    subject: /*#__PURE__*/ v.genericUriString(),
+    subject: /*#__PURE__*/ v.optional(/*#__PURE__*/ v.genericUriString()),
     tags: /*#__PURE__*/ v.optional(
       /*#__PURE__*/ v.constrain(
         /*#__PURE__*/ v.array(
@@ -28,10 +30,11 @@ const _mainSchema = /*#__PURE__*/ v.record(
         [/*#__PURE__*/ v.arrayLength(0, 10)],
       ),
     ),
-    title: /*#__PURE__*/ v.constrain(/*#__PURE__*/ v.string(), [
-      /*#__PURE__*/ v.stringLength(0, 500),
-      /*#__PURE__*/ v.stringGraphemes(0, 50),
-    ]),
+    get titles() {
+      return /*#__PURE__*/ v.constrain(/*#__PURE__*/ v.array(localeSchema), [
+        /*#__PURE__*/ v.arrayLength(1),
+      ]);
+    },
   }),
 );
 const _nsidSchema = /*#__PURE__*/ v.object({
@@ -42,15 +45,19 @@ const _nsidSchema = /*#__PURE__*/ v.object({
   urlSchema: /*#__PURE__*/ v.string(),
 });
 
+type locale$schematype = typeof _localeSchema;
 type main$schematype = typeof _mainSchema;
 type nsid$schematype = typeof _nsidSchema;
 
+export interface localeSchema extends locale$schematype {}
 export interface mainSchema extends main$schematype {}
 export interface nsidSchema extends nsid$schematype {}
 
+export const localeSchema = _localeSchema as localeSchema;
 export const mainSchema = _mainSchema as mainSchema;
 export const nsidSchema = _nsidSchema as nsidSchema;
 
+export interface Locale extends v.InferInput<typeof localeSchema> {}
 export interface Main extends v.InferInput<typeof mainSchema> {}
 export interface Nsid extends v.InferInput<typeof nsidSchema> {}
 

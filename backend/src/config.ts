303450bf6e495f1255f8877602612bf282fe466d
usounds <grave.evarg@gmail.com>
2025-09-06 11:09:32 +0900

    feat : backend

---
+++ backend/src/config.ts
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+
+export const JETSREAM_URL = process.env.JETSREAM_URL ?? 'wss://jetstream2.us-west.bsky.network/subscribe';
+export const BOOKMARK = 'blue.rito.feed.bookmark'
+export const SERVICE = 'blue.rito.service.verification'
+export const CURSOR_UPDATE_INTERVAL =
+  process.env.CURSOR_UPDATE_INTERVAL ? Number(process.env.CURSOR_UPDATE_INTERVAL) : 60000;
---
+++ backend/src/lexicons/index.ts
@@ -0,0 +1,2 @@
+export * as BlueRitoFeedBookmark from "./types/blue/rito/feed/bookmark.js";
+export * as BlueRitoServiceVerification from "./types/blue/rito/service/verification.js";

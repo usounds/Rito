cb1ac401df5872a423aa4203f3e30649d40cd845
usounds <46260022+usounds@users.noreply.github.com>
2025-09-21 20:45:56 +0900

    feat : 利用規約とTOSにパンクズ

--- frontend/app/[locale]/privacy/page.tsx
+++ frontend/app/[locale]/privacy/page.tsx
@@ -1,7 +1,9 @@
 import fs from "fs";
 import path from "path";
 import ReactMarkdown from "react-markdown";
-import { Container} from "@mantine/core";
+import { Container } from "@mantine/core";
+import Breadcrumbs from "@/components/Breadcrumbs";
+import { getTranslations } from "next-intl/server";
 
 interface PageProps {
   params: { locale: string };
@@ -9,6 +11,7 @@ interface PageProps {
 
 export default async function PrivacyPage({ params }: PageProps) {
   const { locale } = await params;
+  const t = await getTranslations({ locale });
 
   // privacy フォルダ内の md を参照
   const filePath = path.join(process.cwd(), "app", "[locale]", "privacy", `${locale}.md`);
@@ -23,6 +26,7 @@ export default async function PrivacyPage({ params }: PageProps) {
 
   return (
     <Container size="md" mx="auto">
+      <Breadcrumbs items={[{ label: t("header.privacypolicy") }]} />
       <ReactMarkdown>{content}</ReactMarkdown>
     </Container>
   );

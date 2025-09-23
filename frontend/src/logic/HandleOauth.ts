7cebdace6cbdece40c59f645af4c87285f20a069
usounds <grave.evarg@gmail.com>
2025-09-24 08:25:43 +0900

    fix : リフレッシュトークンを60日に

--- frontend/src/logic/HandleOauth.ts
+++ frontend/src/logic/HandleOauth.ts
@@ -5,7 +5,7 @@ export const ACCESS_TOKEN_COOKIE = "access_token";
 export const REFRESH_TOKEN_COOKIE = "refresh_token";
 
 export const ACCESS_TOKEN_MAX_AGE = 60 * 60 * 24; // 1日
-export const REFRESH_TOKEN_MAX_AGE = 60 * 60 * 24 * 365; // 365日
+export const REFRESH_TOKEN_MAX_AGE = 60 * 60 * 24 * 60; // 60日
 
 // リフレッシュトークンでアクセストークンを更新
 export async function refreshAccessToken(refreshToken: string) {

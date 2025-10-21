18f9b32183c8533fd924a9f428870baad49a6f24
usounds <46260022+usounds@users.noreply.github.com>
2025-10-21 23:08:28 +0900

    fix : リファクタ

--- frontend/app/api/oauth/callback/route.ts
+++ frontend/app/api/oauth/callback/route.ts
@@ -16,44 +16,44 @@ export async function GET(req: NextRequest) {
   const params = url.searchParams;
 
   const error = params.get("error");
-if (error === "consent_required" || error === "login_required") {
-  // サイレントサインインのリトライ
-const handle = req.cookies.get("HANDLE")?.value;
+  if (error === "consent_required" || error === "login_required") {
+    // サイレントサインインのリトライ
+    const handle = req.cookies.get("HANDLE")?.value;
 
-  if (!handle) return NextResponse.redirect("/");
+    if (!handle) return NextResponse.redirect("/");
 
-  const url = await client.authorize(handle);
-  return NextResponse.redirect(url);
-}
+    const url = await client.authorize(handle);
+    return NextResponse.redirect(url);
+  }
 
   try {
-      // OAuth callback
-      const { session } = await client.callback(params);
+    // OAuth callback
+    const { session } = await client.callback(params);
 
-      // 認証成功
-      const agent = new Agent(session);
-      await agent.getProfile({ actor: agent.did || '' });
-      // もし redirectTo が必要ならクッキーから取得してリダイレクト
-      const redirectTo = req.cookies.get("REDIRECT_TO")?.value || "/";
+    // 認証成功
+    const agent = new Agent(session);
+    await agent.getProfile({ actor: agent.did || '' });
+    // もし redirectTo が必要ならクッキーから取得してリダイレクト
+    const redirectTo = req.cookies.get("REDIRECT_TO")?.value || "/";
 
-      const response = NextResponse.redirect(redirectTo);
+    const response = NextResponse.redirect(redirectTo);
 
-      response.cookies.set("USER_DID", signDid(session.did), {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-        path: "/",
-        maxAge: 60 * 60 * 24 * 60, // 60日
-      });
-      response.cookies.delete({ name: "REDIRECT_TO", path: "/" });
-      response.cookies.delete({ name: "HANDLE", path: "/" });
-      return response
+    response.cookies.set("USER_DID", signDid(session.did), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 60, // 60日
+    });
+    response.cookies.delete({ name: "REDIRECT_TO", path: "/" });
+    response.cookies.delete({ name: "HANDLE", path: "/" });
+    return response
 
   } catch {
 
-      const redirectTo = req.cookies.get("REDIRECT_TO")?.value || "/";
+    const redirectTo = req.cookies.get("REDIRECT_TO")?.value || "/";
 
-      const response = NextResponse.redirect(redirectTo);
-      return response
+    const response = NextResponse.redirect(redirectTo);
+    return response
   }
 }

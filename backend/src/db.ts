97b9984a097fbcbf543676e0764895b7cb03f2bc
usounds <46260022+usounds@users.noreply.github.com>
2026-02-21 10:40:19 +0900

    fix : リカバリー処理

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -6,7 +6,7 @@
 import 'dotenv/config';
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
+import * as pg from "pg";
 import pLimit from "p-limit";
 import logger from './logger.js';
 
